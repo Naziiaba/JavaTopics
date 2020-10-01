c7b4e9366a83bbbcecf3841b5b12d8c50982139c
Naziia Travels <35944002+Naziiaba@users.noreply.github.com>
2020-10-01 15:53:31 -0500

    Add files via upload

---
+++ AdvancedJava/JavaEssentialTraining2019/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 10 + 12;
+        String answer = "The anser is " + result;
+        System.out.println(answer);
+
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+    }
+}
